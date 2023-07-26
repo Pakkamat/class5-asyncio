e0d20b7b23d752442d596a2455f8cb97b7f85d6a
Pakkamat <6310301036@cdti.ac.th>
2023-07-26 14:20:28 +0700

    commit_3

--- 1-3-simple-async-right.py
+++ 1-3-simple-async-right.py
@@ -19,25 +19,31 @@
 import asyncio
 import time
 
+# ใช้ฟังก์ชั่นของ async และใช้ฟังก์ชั่นการรอแบบ async ทำให้โปรแกรมเร็วขึ้น
 async def sleep():
     print(f'Time: {time.time() - start:.2f}')
     await asyncio.sleep(1)
 
+# asyncio สร้างฟังก์ชั่นรับค่าตัวแปร 2 ตัว เป็นชื่อ และ เลข
+# สร้างตัวแปรมาเก็บค่า คือ total
+# ทำการบวกเลข เก็บในตัวแปร total โดยวนลูปจาก ตัวแปร numbers 
 async def sum(name, numbers):
     total = 0
     for number in numbers:
         print(f'Task {name}: Computing {total}+{number}')
         await sleep()
         total += number
+    # Print ชื่อ Task, ผล Sum ที่ได้ (และ enter บรรทัด)
     print(f'Task {name}: Sum = {total}\n')
 
 start = time.time()
-
+# asyncio สร้างฟังก์ชั่นมาเก็บค่า และสร้าง task ขึ้นมา แต่ยังไม่ให้รัน 
 loop = asyncio.get_event_loop()
 tasks = [
     loop.create_task(sum("A", [1, 2])),
     loop.create_task(sum("B", [1, 2, 3])),
 ]
+# ทำการรัน task และทำจนเสร็จ
 loop.run_until_complete(asyncio.wait(tasks))
 loop.close()
 
