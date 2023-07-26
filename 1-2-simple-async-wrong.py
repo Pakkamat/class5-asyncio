93a01260844bf6a578cb35c5bd16f1a4f093a4c5
Pakkamat <6310301036@cdti.ac.th>
2023-07-26 14:18:00 +0700

    commit_1

---
+++ 1-2-simple-async-wrong.py
@@ -0,0 +1,51 @@
+# ผลที่ได้จากการ Run
+
+# Task A: Computing 0+1
+# Time: 0.00
+# Task A: Computing 1+2
+# Time: 1.01
+# Task A: Sum = 3
+
+# Task B: Computing 0+1
+# Time: 2.01
+# Task B: Computing 1+2
+# Time: 3.02
+# Task B: Computing 3+3
+# Time: 4.03
+# Task B: Sum = 6
+
+# Time: 5.04 sec
+
+import asyncio
+import time
+
+# ใช้ฟังก์ชั่นของ async แต่ไปใช้ time.sleep ทำให้การทำงานต้องรอ โปรแกรมจึงช้า
+async def sleep():
+    print(f'Time: {time.time() - start:.2f}')
+    time.sleep(1)
+
+# asyncio สร้างฟังก์ชั่นรับค่าตัวแปร 2 ตัว เป็นชื่อ และ เลข
+# สร้างตัวแปรมาเก็บค่า คือ total
+# ทำการบวกเลข เก็บในตัวแปร total โดยวนลูปจาก ตัวแปร numbers 
+async def sum(name, numbers):
+    total = 0
+    for number in numbers:
+        print(f'Task {name}: Computing {total}+{number}')
+        await sleep()
+        total += number
+    # Print ชื่อ Task, ผล Sum ที่ได้ (และ enter บรรทัด)
+    print(f'Task {name}: Sum = {total}\n')
+
+start = time.time()
+# asyncio สร้างฟังก์ชั่นมาเก็บค่า และสร้าง task ขึ้นมา แต่ยังไม่ให้รัน 
+loop = asyncio.get_event_loop()
+tasks = [
+    loop.create_task(sum("A", [1, 2])),
+    loop.create_task(sum("B", [1, 2, 3])),
+]
+# ทำการรัน task และทำจนเสร็จ
+loop.run_until_complete(asyncio.wait(tasks))
+loop.close()
+
+end = time.time()
+print(f'Time: {end-start:.2f} sec')
